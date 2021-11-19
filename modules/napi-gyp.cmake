ed662f9c989317d4b39a50f2cdee9ef3841fd80e
Mathis Logemann <mathisloge@gmail.com>
2021-11-19 16:25:55 +0100

    correct paths for windows

--- modules/napi-gyp.cmake
+++ modules/napi-gyp.cmake
@@ -1,10 +1,13 @@
-
-set(node_headers_inc_dir 
-    "${node_root_dir}/src"
-    "${node_root_dir}/deps/v8/include"
-    "${node_root_dir}/deps/v8/include"
-    "${node_root_dir}/deps/uv/include"
-)
+if(WIN32)
+    set(node_headers_inc_dir "${node_root_dir}/include/node")
+else()
+    set(node_headers_inc_dir 
+        "${node_root_dir}/src"
+        "${node_root_dir}/deps/v8/include"
+        "${node_root_dir}/deps/v8/include"
+        "${node_root_dir}/deps/uv/include"
+    )
+endif()
 message(STATUS "headers: ${node_headers_inc_dir}")
 set(NAPI_VERSION ${NAPI_VERSION} CACHE STRING "used napi version")
 
