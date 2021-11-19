228cf1645f0b6e9c686967f2d9beb9a2fd74f62a
Mathis Logemann <mathisloge@gmail.com>
2021-11-19 16:20:30 +0100

    fix path

--- modules/napi-gyp.cmake
+++ modules/napi-gyp.cmake
@@ -1,6 +1,6 @@
 
 set(node_headers_inc_dir 
-    "${node_root_dir}/include/src"
+    "${node_root_dir}/src"
     "${node_root_dir}/deps/v8/include"
     "${node_root_dir}/deps/v8/include"
     "${node_root_dir}/deps/uv/include"
