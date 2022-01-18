410b97035455afc79e3fafabe90373070c57cb51
Mathis Logemann <mathisloge@gmail.com>
2022-01-18 17:25:41 +0100

    fix path

--- modules/napi-gyp.cmake
+++ modules/napi-gyp.cmake
@@ -1,18 +1,12 @@
-if(WIN32)
+if(WIN32 OR EXISTS "${node_root_dir}/include/node")
     set(node_headers_inc_dir "${node_root_dir}/include/node")
 else()
-    if(NOT EXISTS "${node_root_dir}/src")
-        set(node_headers_inc_dir 
-            "${node_root_dir}/deps/v8/include"
-            "${node_root_dir}/deps/v8/include"
-            "${node_root_dir}/deps/uv/include",
-            "${node_root_dir}/src"
-        )
-    else()
-        set(node_headers_inc_dir 
-            "${node_root_dir}/include/node"
-        )
-    endif()
+    set(node_headers_inc_dir 
+        "${node_root_dir}/deps/v8/include"
+        "${node_root_dir}/deps/v8/include"
+        "${node_root_dir}/deps/uv/include",
+        "${node_root_dir}/src"
+    )
 endif()
 message(STATUS "headers: ${node_headers_inc_dir}")
 set(NAPI_VERSION ${NAPI_VERSION} CACHE STRING "used napi version")
