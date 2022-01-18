89a01d3d4f5000bb4cba85e48c536eb477e79fd7
Mathis Logemann <mathisloge@gmail.com>
2022-01-18 16:55:55 +0100

    check for src dir

--- modules/napi-gyp.cmake
+++ modules/napi-gyp.cmake
@@ -2,11 +2,13 @@ if(WIN32)
     set(node_headers_inc_dir "${node_root_dir}/include/node")
 else()
     set(node_headers_inc_dir 
-        "${node_root_dir}/src"
         "${node_root_dir}/deps/v8/include"
         "${node_root_dir}/deps/v8/include"
         "${node_root_dir}/deps/uv/include"
     )
+    if(EXISTS "${node_root_dir}/src")
+        set(node_headers_inc_dir ${node_headers_inc_dir} "${node_root_dir}/src")
+    endif()
 endif()
 message(STATUS "headers: ${node_headers_inc_dir}")
 set(NAPI_VERSION ${NAPI_VERSION} CACHE STRING "used napi version")
