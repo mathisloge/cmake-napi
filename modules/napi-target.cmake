cdc5203c12f7e44ccb260b315256590d54b574af
Mathis Logemann <mathisloge@gmail.com>
2022-01-19 10:31:17 +0100

    messages

--- modules/napi-target.cmake
+++ modules/napi-target.cmake
@@ -1,11 +1,13 @@
 message(STATUS "using napi version ${NAPI_VERSION}")
 if(EXISTS "${node_lib_file}")
+    message(STATUS "napi as static lib with ${node_lib_file}")
     add_library(napi STATIC IMPORTED GLOBAL)
     set_target_properties(napi PROPERTIES
         IMPORTED_LOCATION "${node_lib_file}"
         INTERFACE_INCLUDE_DIRECTORIES "${node_headers_inc_dir}"
     )
 else()
+    message(STATUS "napi as header only")
     add_library(napi INTERFACE IMPORTED GLOBAL)
     set_target_properties(napi PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${node_headers_inc_dir}"
